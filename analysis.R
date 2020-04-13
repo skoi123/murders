1162ed973d82356552627041600bb736d76db188
skoi123 <koitasahil@gmail.com>
2020-04-13 18:52:46 +0530

    First commit. Adding README.txt file just to get started

---
+++ analysis.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+load("rda/murders.rda")
+
+
+murders %>% mutate( abb=reorder(abb,rate))%>% ggplot(aes(abb,rate))+geom_bar(stat="identity",width=.5,color="blue")+coord_flip()
+
+ggsave("figs/barplot.png")
