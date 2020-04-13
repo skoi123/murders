623e7f3409ad3c7b94d54748c64496a5ef4ca976
skoi123 <koitasahil@gmail.com>
2020-04-13 19:20:52 +0530

    x is 5

--- analysis.R
+++ analysis.R
@@ -4,4 +4,5 @@ load("rda/murders.rda")
 
 murders %>% mutate( abb=reorder(abb,rate))%>% ggplot(aes(abb,rate))+geom_bar(stat="identity",width=.5,color="blue")+coord_flip()
 
+x<- 5
 ggsave("figs/barplot.png")
